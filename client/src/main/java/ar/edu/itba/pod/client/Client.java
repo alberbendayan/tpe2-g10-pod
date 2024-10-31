89efd8452c332cf530a42626174e5a950331e85d
alberbendayan <108437189+alberbendayan@users.noreply.github.com>
2024-10-31 17:45:15 -0300

    Initial commit

---
+++ client/src/main/java/ar/edu/itba/pod/client/Client.java
@@ -0,0 +1,9 @@
+package ar.edu.itba.pod.client;
+
+
+public class Client {
+
+    public static void main(String[] args) throws InterruptedException {
+
+    }
+}
---
+++ server/src/main/java/ar/edu/itba/pod/server/Server.java
@@ -0,0 +1,10 @@
+package ar.edu.itba.pod.server;
+
+
+import java.io.IOException;
+
+public class Server {
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+
+    }}
